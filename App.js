f40527391417cda6daa3bc9140222bbe2586d60d
tahabisginsoftware <tahabisginsoftware@gmail.com>
2023-02-10 23:58:00 +0100

    Yet Another App :D

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+import Scanner from './screens/Scanner';
+import Home from './screens/Home';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import { StatusBar } from 'expo-status-bar';
+
+const Stack = createStackNavigator();
+
+function App() {
+  return(
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={Home}
+      options={{
+        headerStyle: {
+          backgroundColor: '#313e50'
+        },
+        headerTitleStyle: {
+          fontWeight:'bold'
+        },
+        headerTintColor: '#fff'
+      }} />
+      <Stack.Screen name="Scanner" component={Scanner}
+      options={{
+        headerStyle: {
+          backgroundColor: '#313e50'
+        },
+        headerTitleStyle: {
+          fontWeight:'bold'
+        },
+        headerTintColor: '#fff'
+      }}/>
+    </Stack.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default () => {
+  return(
+    <NavigationContainer>
+      <App/>
+      <StatusBar style='light'/>
+    </NavigationContainer>
+  )
+}
